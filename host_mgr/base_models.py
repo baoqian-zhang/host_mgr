58ca2971210fe7b028b312350bbb2c88cc78e11e
baoqian-zhang <18738845127@163.com>
2026-03-25 09:27:31 +0800

    change id field

--- host_mgr/base_models.py
+++ host_mgr/base_models.py
@@ -1,9 +1,22 @@
+import uuid
+
 from django.db import models
 
 from host_mgr.fields import NullableJSONStringField
 
 
+def uuid_hex_str():
+    return uuid.uuid4().hex
+
+
 class BaseModel(models.Model):
+    id = models.CharField(
+        max_length=32,
+        primary_key=True,
+        default=uuid_hex_str,
+        editable=False,
+        verbose_name="ID",
+    )
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
     updated_at = models.DateTimeField(auto_now=True, verbose_name="更新时间")
     extra = NullableJSONStringField(
